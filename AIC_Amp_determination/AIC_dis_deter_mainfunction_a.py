52227ac542cf07d1261d641fa6c580f990264095
ReonLion <ReonLion@users.noreply.github.com>
2018-02-01 22:50:57 +0800

    Dr Li

---
+++ AIC_Amp_determination/AIC_dis_deter_mainfunction_a.py
@@ -0,0 +1,59 @@
+# coding=utf-8
+import numpy as np
+
+class AIC_Amp_determination():
+    def __init__(self):
+        '''
+        设置Debug模式
+        '''
+        debug_mode = True
+        
+        ssf_paras = np.load('../params/ssf_paras.npz')
+        Amp_DATA = np.load('../params/Amp_DATA.npz')
+        
+        '''
+        测试参数录入
+        '''
+        fc = ssf_paras['fc'] / 1e9                                          # 载波中心频率 Hz
+        c = ssf_paras['c']                                                  # 光速 m/s
+        ATT = ssf_paras['ATT']
+        chirp_num = ssf_paras['chirp_num']                                  # chirp 数目 3秒 1933+1933+1934 (可能会引起后面作图错误)
+        window = ssf_paras['window']                                        # 窗设置 例如此处设为 20 lambda
+        beg_p = ssf_paras['beg_p']                                          # 数据起始点设置 从第二个 chirp开始 (由于第一个chirp不对所有的RSL数据都比RX位置数据多了1, 但此处也要注意) 2对应了GPS坐标1
+        end_p = ssf_paras['end_p']                                          # 数据终止点设置 32对应了GPS坐标31
+        beg_t_mark = ssf_paras['beg_t_mark']                                # 起始点时间序号 例如此数据共50s 因此 此时值为 0
+        cable1 = ssf_paras['cable1']                                        # 线损 3dB
+        cable2 = ssf_paras['cable2']                                        # 线损 3dB
+        TX_power = ssf_paras['TX_power']                                    # 天线发射功率 16 dBm
+        G_TX = ssf_paras['TX_Gain']                                         # 发射天线增益 2 dBi
+        G_RX = ssf_paras['RX_Gain']                                         # 接收天线增益 2 dBi
+        h_1 = ssf_paras['TX_heigh'] / 1000                                  # 发射天线有效高度 km
+        h_2 = ssf_paras['RX_heigh'] / 1000                                  # 接收天线有效高度 km
+        SM_time = ssf_paras['SM_time']
+        SM_distance = ssf_paras['SM_distance']
+        TIME = ssf_paras['TIME'] - 1
+        data_x = Amp_DATA['AIC_DATA']
+        
+        '''
+        运行程序开始
+        '''
+        area_num = np.max(data_x.shape)
+        start_p = 1
+        end_p = area_num
+        
+        for num in range(0, area_num):
+            X = []
+        
+        
+        '''
+        debug message
+        '''
+        if debug_mode:
+            print('data_x')
+            print(data_x.shape)
+            print(area_num)
+            
+        
+        
+if __name__ == '__main__':
+    AIC_Amp_determination()
--- Main_function_and_doppler.py
+++ Main_function_and_doppler.py
@@ -119,7 +119,7 @@ class main_doppler():
             D_window = np.concatenate((D_window, distance[i + 1]), axis = 0)
             TIME = np.concatenate((TIME, time[i + 1]), axis = 0)
             V_Rel = np.concatenate((V_Rel, V_rel[i + 1]), axis = 0)
-        CIR_window_doppler = after_window_cir[0 : np.shape(TIME)[0]]
+        CIR_window_doppler = after_window_cir[0 : np.max(np.shape(TIME))]
         after_window_cir = []
         CIR_1 = []
         
@@ -169,7 +169,7 @@ class main_doppler():
         for num in range(1, num_sm1 - 1):
             Equ_Narr_band = np.concatenate((Equ_Narr_band, CIR_window_doppler[num]), axis = 0)
             
-        Narrow_band_signal = ATT * np.abs(np.power(Equ_Narr_band[0:np.shape(SM_time)[0], :], 2))
+        Narrow_band_signal = ATT * np.abs(np.power(Equ_Narr_band[0:np.max(np.shape(SM_time)), :], 2))
         NB_signal = 10 * np.log10(np.sum(Narrow_band_signal, axis = 1) / C)
         PL_no_SM = 10 * np.log10(PL_no_sm)
         Small_scale_fading = NB_signal - PL_no_SM
@@ -221,7 +221,7 @@ class main_doppler():
         # 具体秒数多普勒频移
         fre_in_domain = 10 * np.log10(ATT * np.power(np.abs(np.fft.fftshift(np.fft.fft(CIR_doppler, axis=0), axes=(0, ))), 2))
         Fre_innor_domain = fre_in_domain - np.max(np.max(fre_in_domain))                                # 归一化操作
-        Fre_resolution = np.floor(chirp_num) / (win_wide * np.arange(d_begp, d_endp + 0.1).shape[0])
+        Fre_resolution = np.floor(chirp_num) / (win_wide * np.max(np.arange(d_begp, d_endp + 0.1).shape))
         # 瞬时多普勒频移再翻转
         Hz_in_x = np.flipud(np.arange(-np.floor(chirp_num / 2), np.floor(chirp_num / 2) + Fre_resolution / 10e4, Fre_resolution))
         RSL = 10 * np.log10(np.sum(pdp_window, axis = 1) / np.shape(pdp_window)[-1])                    # RSL units: dBm
@@ -336,7 +336,7 @@ class main_doppler():
         X = np.arange(t_res, t_res * 2560 + t_res / 10e4, t_res)
         Y = Hz_in_x
         X, Y = np.meshgrid(X, Y)
-        Z = Fre_innor_domain[0 : np.shape(Hz_in_x)[0], :]
+        Z = Fre_innor_domain[0 : np.max(np.shape(Hz_in_x)), :]
     
         fig = plt.figure(tight_layout = True)
         ax = fig.add_subplot(111)
@@ -409,7 +409,8 @@ class main_doppler():
         np.savez('./params/ssf_paras.npz', fc = fc, c = c, ATT = ATT, chirp_num = chirp_num, window = window, 
                  beg_p = beg_p, end_p = end_p, beg_t_mark = beg_t_mark, cable1 = cable1, cable2 = cable2, 
                  TX_power = TX_power, TX_Gain = TX_Gain, RX_Gain = RX_Gain, TX_heigh = TX_heigh, RX_heigh = RX_heigh,
-                 TIME = TIME, D_window = D_window, v_rx = v_rx, v_tx = v_tx, win_wide = win_wide)
+                 TIME = TIME, D_window = D_window, v_rx = v_rx, v_tx = v_tx, win_wide = win_wide, SM_time = SM_time,
+                 SM_distance = SM_distance)
         np.savez('./params/ssf_data.npz', NB_signal = NB_signal, PL_no_SM = PL_no_SM, Small_scale_fading = Small_scale_fading)
         np.savez('./params/Amp_DATA.npz', AIC_DATA = AIC_DATA)
         np.savez('./params/PL_paras.npz', fc = fc, c = c, ATT = ATT, chirp_num = chirp_num, window = window, 
