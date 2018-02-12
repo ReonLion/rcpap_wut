c8bd6c038962ed520f2414f34b4056d80cf328b5
ReonLion <ReonLion@users.noreply.github.com>
2018-02-12 14:30:40 +0800

    Dr Li

--- Doppler analysis/rmsdop_main_function.py
+++ Doppler analysis/rmsdop_main_function.py
@@ -61,7 +61,129 @@ class rmsdop():
         Drms = np.array(Drms)
         
         h1 = (np.max(Dm, axis=0) - np.min(Dm, axis=0)) / (1 + np.log2(Dm.shape[0]))
+        h2 = (np.max(Drms, axis=0) - np.min(Drms, axis=0)) / (1 + np.log2(Drms.shape[0]))
+        num1 = (np.max(Dm, axis=0) - np.min(Dm, axis=0)) / h1
+        num2 = (np.max(Drms, axis=0) - np.min(Drms, axis=0)) / h2
         
+        a1, b1_cache = np.histogram(Dm, int(np.ceil(num1)))
+        b1 = []
+        for i in range(0, b1_cache.shape[0] - 1):
+            b1.append((b1_cache[i] + b1_cache[i + 1]) / 2)
+        b1 = np.array(b1)
+        
+        a2, b2_cache = np.histogram(Drms, int(np.ceil(num2)))
+        b2 = []
+        for i in range(0, b2_cache.shape[0] - 1):
+            b2.append((b2_cache[i] + b2_cache[i + 1]) / 2)
+        b2 = np.array(b2)
+        
+        A1 = a1 / np.sum(a1, axis = 0)
+        A2 = a2 / np.sum(a2, axis = 0)
+        
+        '''
+        图形生成程序
+        '''
+        '''
+        绘制保存fig3
+        '''
+        fig = plt.figure(tight_layout = True)
+        ax = fig.add_subplot(111)
+        
+        ax.plot(TIME, Dm, 'b', linewidth = 2)
+        
+        ax.set_xlim(np.min(TIME), np.max(TIME))
+        ax.set_ylim(np.min(Dm), np.max(Dm))
+        ax.set_xlabel('Time in s', fontproperties = 'Times New Roman', fontsize = 10)
+        ax.set_ylabel('Mean Doppler in Hz', fontproperties = 'Times New Roman', fontsize = 10)
+        ax.grid(True)
+        
+        plt.savefig('../results/power_domain/rmsdop_main_function_fig3.png')
+        
+        '''
+        绘制保存fig4
+        '''
+        fig = plt.figure(tight_layout = True)
+        ax = fig.add_subplot(111)
+    
+        ax.plot(TIME, Drms, 'b', linewidth = 2)
+    
+        ax.set_xlim(np.min(TIME), np.max(TIME))
+        ax.set_ylim(np.min(Drms), np.max(Drms))
+        ax.set_xlabel('Time in s', fontproperties = 'Times New Roman', fontsize = 10)
+        ax.set_ylabel('RMS Doppler in Hz', fontproperties = 'Times New Roman', fontsize = 10)
+        ax.grid(True)
+    
+        plt.savefig('../results/power_domain/rmsdop_main_function_fig4.png')
+        
+        '''
+        绘制保存fig6
+        '''
+        fig = plt.figure(tight_layout = True)
+        ax = fig.add_subplot(111)
+        
+        Dm_sort = np.sort(Dm)
+        count = Dm.shape[0]
+        Dm_cdf = [[], []]
+        for i in range(0, count):
+            Dm_cdf[0].append(Dm_sort[i])
+            Dm_cdf[1].append((i + 1) / count)
+        
+        ax.step(Dm_cdf[0], Dm_cdf[1], 'b', linewidth = 2.0)
+        
+        ax.set_xlabel('RMS Doppler in Hz', fontproperties = 'Times New Roman', fontsize = 10)
+        ax.set_ylabel('CDF of mean Doppler', fontproperties = 'Times New Roman', fontsize = 10)
+        ax.grid(True)
+    
+        plt.savefig('../results/power_domain/rmsdop_main_function_fig6.png')
+        
+        '''
+        绘制保存fig8
+        '''
+        fig = plt.figure(tight_layout = True)
+        ax = fig.add_subplot(111)
+        
+        Drms_sort = np.sort(Drms)
+        count = Drms.shape[0]
+        Drms_cdf = [[], []]
+        for i in range(0, count):
+            Drms_cdf[0].append(Drms_sort[i])
+            Drms_cdf[1].append((i + 1) / count)
+        
+        ax.step(Drms_cdf[0], Drms_cdf[1], 'b', linewidth = 2.0)
+        
+        ax.set_xlabel('RMS Doppler in Hz', fontproperties = 'Times New Roman', fontsize = 10)
+        ax.set_ylabel('CDF of RMS Doppler', fontproperties = 'Times New Roman', fontsize = 10)
+        ax.grid(True)
+    
+        plt.savefig('../results/power_domain/rmsdop_main_function_fig8.png')
+        
+        '''
+        绘制保存fig9
+        '''
+        fig = plt.figure(tight_layout = True)
+        ax = fig.add_subplot(111)
+    
+        ax.bar(b1, A1, color = 'b', width = 6)
+        
+        ax.set_xlabel('Mean Doppler in Hz', fontproperties = 'Times New Roman', fontsize = 10)
+        ax.set_ylabel('PDF of mean Doppler', fontproperties = 'Times New Roman', fontsize = 10)
+        ax.grid(True)
+    
+        plt.savefig('../results/power_domain/rmsdop_main_function_fig9.png')
+        
+        '''
+        绘制保存fig10
+        '''
+        fig = plt.figure(tight_layout = True)
+        ax = fig.add_subplot(111)
+    
+        ax.bar(b2, A2, color = 'b', width = 6)
+    
+        ax.set_xlabel('RMS Doppler in Hz', fontproperties = 'Times New Roman', fontsize = 10)
+        ax.set_ylabel('PDF of RMS Doppler', fontproperties = 'Times New Roman', fontsize = 10)
+        ax.grid(True)
+    
+        plt.savefig('../results/power_domain/rmsdop_main_function_fig10.png')
         
         '''
         debug message
@@ -80,6 +202,18 @@ class rmsdop():
             print(Drms.shape)
             print(Drms[0:6])
             print(Drms[-6:])
+            print('h1, h2, num1, num2')
+            print(h1, h2, num1, num2)
+            print('A1')
+            print(A1)
+            print('b1')
+            print(b1)
+            print('A2')
+            print(A2)
+            print('b2')
+            print(b2)
+            
+            
 
 if __name__ == '__main__':
     begin_time = timeit.default_timer()
